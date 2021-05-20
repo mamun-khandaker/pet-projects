b7a3870f3feb6d08f4e98ce66b9767971ea04a1c
Mamun <mamun-khandaker@users.noreply.github.com>
2021-05-20 14:01:24 +0600

    Feat: Completed birthday reminder

--- src/projects/birthday-reminder/BirthdayReminder.jsx
+++ src/projects/birthday-reminder/BirthdayReminder.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import './birthday-reminder.scss';
+import React, { useState } from 'react';
+import BirthdayList from './BirthdayList';
+import BirthdayData from './BirthdayData';
 
 const BirthdayReminder = () => {
+  const [data, setData] = useState(BirthdayData);
+
+  const handleClick = () => {
+    setData([]);
+  }
+
   return (
     <div className="birthday-reminder">
-      Birthday reminder
+      <h2 className="title">
+        <span>Birthday reminder</span>
+      </h2>
+
+      <div className="birthday-box">
+        <h3 className="birthday-title">{data.length} birthdays today</h3>
+
+        <BirthdayList people={data} />
+
+        <button className="button" onClick={handleClick}>Clear All</button>
+      </div>
     </div>
   )
 }
